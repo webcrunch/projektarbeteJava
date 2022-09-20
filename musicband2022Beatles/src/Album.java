f257fc8cac969144ba8035f1a710afeaabd5a4b2
jarl Lindquist <jarllinduist@gmail.com>
2022-09-20 14:42:25 +0200

    Start creating Album class

---
+++ musicband2022Beatles/src/Album.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Album {
+    public String name, info;
+    public int year;
+    public Album(String name, String info, int year){
+        this.name = name;
+        this.info = info;
+        this.year = year;
+    }
+
+
+}
