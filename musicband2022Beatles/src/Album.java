5dc8bbd64f1efe6e10672c35060c72a08b47ce00
jarl Lindquist <jarllinduist@gmail.com>
2022-09-24 09:49:11 +0200

    Creation of a new album. And some test methods and a arraylist to be able to test it.

--- musicband2022Beatles/src/Album.java
+++ musicband2022Beatles/src/Album.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
 public class Album {
-    public String name, info;
+    public String name;
+    public String info;
     public int year;
+    // Remove the arraylist??. Only using it to test so the creation of album is working...
+    public ArrayList<Album> albums = new ArrayList<>();
     public Album(String name, String info, int year){
         this.name = name;
         this.info = info;
         this.year = year;
     }
 
-
-
-
+    // for the test of creation of Album
+    public void setAlbum(Album album){
+        albums.add(album);
+    }
+    // for the test of creation of Album
+    public void displayAllBands(){
+        for (Album album: albums){
+            System.out.println(album.name);
+        }
+    }
 }
--- musicband2022Beatles/src/Main.java
+++ musicband2022Beatles/src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+
+        // creating a new album with dummy data and test if it will work.
+        Album testAlbum = new Album("AlbumTest", "this is a structural base of the beast", 2022);
+        testAlbum.setAlbum(testAlbum);
+        testAlbum.displayAllBands();
+    }
 }
