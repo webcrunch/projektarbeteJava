6387fdeb3e0dda919f83f390456c6491e84a5a62
jarl Lindquist <jarllinduist@gmail.com>
2022-09-26 14:34:31 +0200

    added help library and starting with the menu

--- musicband2022Beatles/src/Main.java
+++ musicband2022Beatles/src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
+        startMenu();
         // creating a new album with dummy data and test if it will work.
-        Album testAlbum = new Album("AlbumTest", "this is a structural base of the beast", 2022);
+        /*Album testAlbum = new Album("AlbumTest", "this is a structural base of the beast", 2022);
         testAlbum.setAlbum(testAlbum);
         testAlbum.displayAllBands();
+        testAlbum.removeAlbum(testAlbum);
+        testAlbum.displayAllBands(); */
+
+    }
+
+
+    private static void startMenu(){
+        String topChoose = Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker");
+        System.out.println(topChoose);
+    }
+
+    private static void subMenu(){
+
     }
 }
