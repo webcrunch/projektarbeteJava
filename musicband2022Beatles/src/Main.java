318cf53e332a6f758f9867b7eb436ffb3c0985f7
jarl Lindquist <jarllinduist@gmail.com>
2022-09-26 15:10:43 +0200

    working on a main menu and depending on the choose make actions

--- musicband2022Beatles/src/Main.java
+++ musicband2022Beatles/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        startMenu();
+        mainMenu();
         // creating a new album with dummy data and test if it will work.
         /*Album testAlbum = new Album("AlbumTest", "this is a structural base of the beast", 2022);
         testAlbum.setAlbum(testAlbum);
@@ -11,12 +11,25 @@ public class Main {
     }
 
 
-    private static void startMenu(){
-        String topChoose = Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker");
-        System.out.println(topChoose);
+    private static void mainMenu(){ //mainMenu
+        switch(Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa")){
+            case "Lägg till":
+               handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Lägg till");
+                break;
+            case "ta bort":
+                handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Ta bort");
+                break;
+            case "Visa":
+                handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Visa");
+                break;
+            default:
+                //System.out.println("That wasn't an option!");
+        }
+
     }
 
-    private static void subMenu(){
+    private static void handlingChooses(String members, String action ){ // change name ?? menuOptions
+       //String subChoose = Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa");
 
     }
 }
