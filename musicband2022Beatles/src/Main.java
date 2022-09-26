8a79ca34d11c5c280d0eb9eaee9026344eae99f4
jarl Lindquist <jarllinduist@gmail.com>
2022-09-26 15:42:52 +0200

    WIP: menu handling for the lower choose in the menu

--- musicband2022Beatles/src/Main.java
+++ musicband2022Beatles/src/Main.java
@@ -14,22 +14,20 @@ public class Main {
     private static void mainMenu(){ //mainMenu
         switch(Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa")){
             case "Lägg till":
-               handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Lägg till");
+               handlingChooses(Input.menu("Vad vill du Lägga till?", "Band", "Album", "Musiker"), "Lägg till");
                 break;
             case "ta bort":
-                handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Ta bort");
+                handlingChooses(Input.menu("Vad vill du ta bort?", "Band", "Album", "Musiker"), "Ta bort");
                 break;
             case "Visa":
-                handlingChooses(Input.menu("Välj vad du vill arbeta med? ", "Band", "Album", "Musiker"), "Visa");
+                handlingChooses(Input.menu("Vad vill du Visa?" , "Band", "Album", "Musiker"), "Visa");
                 break;
             default:
-                //System.out.println("That wasn't an option!");
         }
 
     }
 
     private static void handlingChooses(String members, String action ){ // change name ?? menuOptions
-       //String subChoose = Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa");
 
     }
 }
