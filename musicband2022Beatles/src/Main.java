98e5385ae915196772ce6b33457226514eb1ff0e
jarl Lindquist <jarllinduist@gmail.com>
2022-09-27 11:53:41 +0200

    created a menu for the different chooses. Need to add functions for the chooses also.

--- musicband2022Beatles/src/Main.java
+++ musicband2022Beatles/src/Main.java
@@ -19,20 +19,49 @@ public class Main {
     private static void mainMenu(){ //mainMenu
         switch(Input.menu("Vad vill du göra? ", "Lägg till", "Ta bort", "Visa")){
             case "Lägg till":
-               handlingChooses(Input.menu("Vad vill du Lägga till?", "Band", "Album", "Musiker"), "Lägg till");
+               handlingActions(Input.menu("Vad vill du Lägga till?", "Band", "Album", "Musiker"), "Lägg till");
                 break;
             case "ta bort":
-                handlingChooses(Input.menu("Vad vill du ta bort?", "Band", "Album", "Musiker"), "Ta bort");
+                handlingActions(Input.menu("Vad vill du ta bort?", "Band", "Album", "Musiker"), "Ta bort");
                 break;
             case "Visa":
-                handlingChooses(Input.menu("Vad vill du Visa?" , "Band", "Album", "Musiker"), "Visa");
+                handlingActions(Input.menu("Vad vill du Visa?" , "Band", "Album", "Musiker"), "Visa");
                 break;
             default:
+                break;
         }
 
     }
 
-    private static void handlingChooses(String members, String action ){ // change name ?? menuOptions
-
+    private static void handlingActions(String members, String action ){ // change name ?? menuOptions
+        switch (members){
+            case "Band":
+                if(action.equals("Visa")){
+                    System.out.println("display on band");
+                } else if (action.equals("Lägg till")) {
+                    System.out.println("Add a band");
+                }else{
+                    System.out.println("Otherwise remove band");
+                }
+                break;
+            case "Album":
+                if(action.equals("Visa")){
+                    System.out.println("display album");
+                } else if (action.equals("Lägg till")) {
+                    System.out.println("add Album");
+                }else{
+                    System.out.println("otherwise remove album");
+                }
+                break;
+            case "Musiker":
+                if(action.equals("Visa")){
+                    System.out.println("Display Musiker");
+                } else if (action.equals("Lägg till")) {
+                    System.out.println("add Musiker");
+                }else{
+                    System.out.println("otherwise remove Musiker");
+                }
+                break;
+        }
     }
 }
